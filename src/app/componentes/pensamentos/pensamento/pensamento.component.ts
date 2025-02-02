e85c8fbf19aa57b7df580bba642a8796bf672c2b
lucas bastos <lucas.bastos.nas@gmail.com>
2025-02-02 18:57:38 -0300

    Finalizando o projeto

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,5 +1,5 @@
+import { Pensamento } from './../pensamento';
 import { Component, Input, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-pensamento',
@@ -10,19 +10,21 @@ export class PensamentoComponent implements OnInit {
 
   @Input() pensamento: Pensamento = {
     id: 0,
-    conteudo: 'vamos programar em angular',
-    autoria: 'lucas',
+    conteudo: 'I love Angular',
+    autoria: 'Nay',
     modelo: 'modelo3'
   }
+
   constructor() { }
 
   ngOnInit(): void {
   }
-  larguraPensamento(): string{
-    if(this.pensamento.conteudo.length >= 256){
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256) {
       return 'pensamento-g'
-    }else{
-      return 'pensamento-p'
     }
+    return 'pensamento-p'
   }
+
 }
